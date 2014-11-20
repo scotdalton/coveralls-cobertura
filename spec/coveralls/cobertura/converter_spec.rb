a6bc2179e3ae4343b044f59d061599e0958327cd
Scot Dalton <scotdalton@gmail.com>
2014-11-20 11:09:38 -0500

    Use the "is_expected.to" RSpec syntax rather than "should"

--- spec/coveralls/cobertura/converter_spec.rb
+++ spec/coveralls/cobertura/converter_spec.rb
@@ -4,17 +4,17 @@ module Coveralls
     describe Converter do
       let(:filename) { "#{File.dirname(__FILE__)}/../../support/cobertura.xml"}
       subject(:converter) { Converter.new(filename) }
-      it { should be_a Converter }
+      it { is_expected.to be_a Converter }
 
       describe '#filename' do
         subject { converter.filename }
-        it { should eq filename }
+        it { is_expected.to eq filename }
       end
 
       context "when the filename doesn't exist" do
         let(:filename) { "/does/not/exist" }
         it 'should raise an ArgumentError' do
-          expect { subject }.to raise_error ArgumentError
+          expect { is_expected.to }.to raise_error ArgumentError
         end
       end
     end
